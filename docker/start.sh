22613fa4c2ddb8f4dec568c2abc2491be5b3ba5c
andirgaarya <andirgaarya@gmail.com>
2026-05-08 16:33:45 +0700

    debug: simplify start.sh

--- docker/start.sh
+++ docker/start.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
 
-# Clear all cache first
-php artisan config:clear
-php artisan cache:clear
-
-# Cache Laravel config
-php artisan config:cache
-php artisan route:cache
-php artisan view:cache
-
-# Storage link & permissions
-php artisan storage:link --force 2>/dev/null || true
-chmod -R 775 /var/www/storage /var/www/bootstrap/cache
-chown -R www-data:www-data /var/www/storage /var/www/bootstrap/cache
-
 # Run migrations
 php artisan migrate --force
 
